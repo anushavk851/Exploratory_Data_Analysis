f34412968d54658c5df2b82bf0b7eed7c33731e5
Anusha VK <anushavk851@gmail.com>
2026-09-15 12:17:25 +0530

    Add data importing functionality for analysis

---
+++ Super Store Analysis/Importing.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+df=pd.read_csv(r'C:\Users\ACCURATE\Desktop\Data Science\EDA\Superstore Sales\superstoresales.csv')
