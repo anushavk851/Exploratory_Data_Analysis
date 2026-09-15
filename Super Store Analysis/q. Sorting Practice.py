b856a3830b494552d4ae3a7a5db7b29fbb9ab6e4
Anusha VK <anushavk851@gmail.com>
2026-09-15 21:15:37 +0530

    Create q. Sorting Practice.py

---
+++ Super Store Analysis/q. Sorting Practice.py
@@ -0,0 +1,18 @@
+# Sorting Practice
+
+#1 Sales ascending
+print(df['Sales'].sort_values())
+#2 Sales descending
+print(df['Sales'].sort_values(ascending=False))
+#3 Order Date
+print(df['Order Date'].sort_values())
+#4 Customer Name
+print(df['Customer Name'].sort_values())
+#5 State
+print(df['State'].sort_values())
+#6 Category
+print(df['Category'].sort_values())
+#7 Sub-Category
+print(df['Sub-Category'].sort_values())
+#8 Product Name
+print(df['Product Name'].sort_values())
