512dfe58e450449393e35380a85afacc2d1e6ed8
Anusha VK <anushavk851@gmail.com>
2026-09-15 12:20:37 +0530

    Create 4 basic sales analysis.py

---
+++ Super Store Analysis/4 basic sales analysis.py
@@ -0,0 +1,14 @@
+#4 BASIC SALES ANALYSIS
+
+#1 Total Sales
+print("Total sales:",df['Sales'].sum())
+#2 Average Sales
+print("Average Sales:",df['Sales'].mean())
+#3 Minimum Sales
+print("Minimum sales:",df['Sales'].min())
+#4 Maximum Sales
+print("Maximum Sales:",df['Sales'].max())
+#5 Median Sales
+print("Median Sales:",df['Sales'].median())
+#6 Number of sales record
+print("Number of sales record:",df['Sales'].count())
