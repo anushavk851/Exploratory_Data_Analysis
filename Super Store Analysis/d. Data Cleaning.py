9df2692935581d691827fc4bf161f6f4884aa0cf
Anusha VK <anushavk851@gmail.com>
2026-09-16 12:10:28 +0530

    Update d. Data Cleaning.py

--- Super Store Analysis/d. Data Cleaning.py
+++ Super Store Analysis/d. Data Cleaning.py
@@ -1,14 +1,24 @@
-#3. DATA CLEANING
+#DATA CLEANING
 
 #1 checking missing values or null values
-print(df.isnull().all())
+print(df.isnull().sum())
+
 #2 check duplicates
 print(df.duplicated().sum())
+
 #3 drop duplicate values
 df=df.drop_duplicates()
 print(df)
+
 #4 Convert date columns If dates are stored as strings
 print(df[['Order Date','Ship Date']].dtypes)
-df['Order Date']=pd.to_datetime(df['Order Date'],format='%d/%m/%Y')
-df['Ship Date']=pd.to_datetime(df['Ship Date'],format='%d/%m/%Y')
+df['Order Date']=pd.to_datetime(df['Order Date'],format='mixed')
+df['Ship Date']=pd.to_datetime(df['Ship Date'],format='mixed')
 print(df.dtypes)
+
+#5 Shipping Date should be after Order Date.
+df['Shipping Duration'] = (df['Ship Date'] - df['Order Date']).dt.days
+print(df[df['Shipping Duration'] < 0][['Order Date', 'Ship Date', 'Shipping Duration']])
+# Remove invalid shipping durations
+df = df[df['Shipping Duration'] >= 0]
+print(df)
