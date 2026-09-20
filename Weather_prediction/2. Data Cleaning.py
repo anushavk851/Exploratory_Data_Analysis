ee537dea52599de7dbce21a8ebe9ee9641d3e43d
Anusha VK <anushavk851@gmail.com>
2026-09-20 14:23:37 +0530

    Create 2. Data Cleaning.py

---
+++ Weather_prediction/2. Data Cleaning.py
@@ -0,0 +1,15 @@
+#Data Cleaning
+
+#1 changing datatype from int to datetime
+df['date']=pd.to_datetime(df['date'],format='mixed')
+print(df.dtypes)
+df['Month']=df['date'].dt.month
+df['Dayofyear']=df['date'].dt.day_of_year
+df=df.drop('date',axis=1)
+print(df.columns)
+
+#2 Null value check
+print(df.isnull().sum()) #no null value
+
+#3 duplicates
+print(df.duplicated().sum())  #no duplicates
