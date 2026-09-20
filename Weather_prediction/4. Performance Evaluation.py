226fef5f2acf05de6d7b0f9e299d5d5ea260c198
Anusha VK <anushavk851@gmail.com>
2026-09-20 14:35:09 +0530

    Enhance performance evaluation metrics in script
    Added calculations for recall, precision, and F1 scores to evaluate model performance.

--- Weather_prediction/4. Performance Evaluation.py
+++ Weather_prediction/4. Performance Evaluation.py
@@ -10,7 +10,25 @@ cmd=ConfusionMatrixDisplay(cm,display_labels=labels)
 cmd.plot()
 
 #B. Accuracy score
-
 from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(ytest, ypred)
 print("Accuracy:", accuracy)
+
+#C. Recall score
+from sklearn.metrics import recall_score
+recall=recall_score(ytest,ypred,average="weighted")
+recall
+
+#D. Precision Score
+from sklearn.metrics import precision_score
+pre=precision_score(ytest,ypred,average="weighted")
+pre
+
+#E. F1 Score
+from sklearn.metrics import f1_score
+score=f1_score(ytest,ypred,average="weighted")
+score
+
+
+
+
