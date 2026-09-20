05bf01bb95a65a5418031b240266c1d00bc404f0
Anusha VK <anushavk851@gmail.com>
2026-09-20 14:37:32 +0530

    Add recall, precision, and F1 score evaluations

--- iris_detection_prediction/4. Performance Evaluation.py
+++ iris_detection_prediction/4. Performance Evaluation.py
@@ -14,3 +14,18 @@ cmd.plot()
 from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(ytest, ypred)
 print("Accuracy:", accuracy)
+
+#C. Recall score
+from sklearn.metrics import recall_score
+recall=recall_score(ytest,ypred,average="weighted")
+recall
+
+#D. Precision Score
+from sklearn.metrics import precision_score
+pre=precision_score(ytest,ypred,average="weighted")
+pre
+
+#E. F1 Score
+from sklearn.metrics import f1_score
+score=f1_score(ytest,ypred,average="weighted")
+score
