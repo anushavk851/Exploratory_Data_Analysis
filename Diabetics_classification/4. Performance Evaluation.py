32ee4e844a2aa6d7af339c574dafb5f6b942d893
Anusha VK <anushavk851@gmail.com>
2026-09-20 14:59:03 +0530

    Implement performance evaluation metrics in script
    Added performance evaluation metrics including accuracy, recall, precision, and F1 score.

---
+++ Diabetics_classification/4. Performance Evaluation.py
@@ -0,0 +1,31 @@
+#Performance Evaluation
+
+#A. Confusion Matrix
+from sklearn.metrics import  confusion_matrix
+cm=confusion_matrix(ytest,ypred)
+print(cm
+ #for displaying as heatmap
+from sklearn.metrics import  ConfusionMatrixDisplay
+cmd=ConfusionMatrixDisplay(cm)
+cmd.plot()
+
+#B. Accuracy score
+#MODEL PERFORMANCE--acc=(TP+TN)/(TP+FP+TN+FN)
+from sklearn.metrics import accuracy_score
+acc=accuracy_score(ytest,ypred)
+acc
+
+#C. RECALL
+from sklearn.metrics import recall_score
+recall=recall_score(ytest,ypred)
+recall
+
+#D. PRECISION SCORE
+from sklearn.metrics import precision_score
+pre=precision_score(ytest,ypred)
+pre
+
+#E. F1 Score
+from sklearn.metrics import f1_score
+score1=f1_score(ytest,ypred)
+score1
