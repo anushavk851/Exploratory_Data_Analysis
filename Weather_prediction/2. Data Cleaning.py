49b39d8c411bbc49ada3f3047f618dc9f78b6b83
Anusha VK <anushavk851@gmail.com>
2026-09-20 19:18:53 +0530

    Add Year column from date in data cleaning

--- Weather_prediction/2. Data Cleaning.py
+++ Weather_prediction/2. Data Cleaning.py
@@ -5,6 +5,7 @@ df['date']=pd.to_datetime(df['date'],format='mixed')
 print(df.dtypes)
 df['Month']=df['date'].dt.month
 df['Dayofyear']=df['date'].dt.day_of_year
+df['Year']=df['date'].dt.year
 df=df.drop('date',axis=1)
 print(df.columns)
 
