5e4e521ae264624e2cf04a6880dfc71d8a2f1701
Anusha VK <anushavk851@gmail.com>
2026-09-23 15:09:23 +0530

    Update 3. Encoding.py

--- ThyroidPrediction_Class_NB/3. Encoding.py
+++ ThyroidPrediction_Class_NB/3. Encoding.py
@@ -1,8 +1,10 @@
-#3. Encoding 
+#Label Encoding 
 
 from sklearn.preprocessing import LabelEncoder
 le=LabelEncoder()
-for i in df.columns:
-  if i!='Age':
-      df[i]=le.fit_transform(df[i])
+obj=df.select_dtypes(include=object).columns.to_list()
+
+for i in obj:
+    df[i] = le.fit_transform(df[i])
+
 df
