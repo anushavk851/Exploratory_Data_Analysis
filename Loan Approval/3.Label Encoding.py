9e03f575372da98fb9f0a70d199f68a1e3f94aa1
Anusha VK <anushavk851@gmail.com>
2026-09-24 11:28:13 +0530

    Add label encoding for categorical features

---
+++ Loan Approval/3.Label Encoding.py
@@ -0,0 +1,8 @@
+#LABEL ENCODING
+
+from sklearn.preprocessing import  LabelEncoder
+le=LabelEncoder()
+obj=df.select_dtypes(include='object').columns.to_list()
+for i in obj:
+  df[i]=le.fit_transform(df[i])
+df
